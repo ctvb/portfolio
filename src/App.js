4d6a1a40e86f5cc01f21764dc75ca7bc1ef09c94
ctvb <deenicks@hotmail.com>
2023-05-12 13:39:53 -0500

    recommit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Navbar from './components/navbar';
+
+function App() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <About />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <Navbar handlePageChange={handlePageChange}></Navbar>
+      {renderPage()}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+export default function About() {
+    return (
+        <div> About page</div>
+    )
+}
